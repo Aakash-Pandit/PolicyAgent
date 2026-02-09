aece2c134960135d639a69428c2ca71ea0306208
Aakash-Pandit <aakashpandit366@gmail.com>
2026-02-09 19:50:37 +0530

    updates function

--- organizations/constants.py
+++ organizations/constants.py
@@ -1,14 +1,12 @@
-def get_organization_function_map():
-    from organizations.db import (
-        get_all_organizations,
-        get_organization_by_name,
-        get_policies_for_organization,
-        get_policy_details,
-    )
+from organizations.db import (
+    get_organization_details,
+    get_policies_for_organization,
+    get_policy_details,
+)
 
+def get_organization_function_map():
     return {
-        "get_organization_by_name": get_organization_by_name,
-        "get_all_organizations": get_all_organizations,
+        "get_organization_details": get_organization_details,
         "get_policies_for_organization": get_policies_for_organization,
         "get_policy_details": get_policy_details,
     }
